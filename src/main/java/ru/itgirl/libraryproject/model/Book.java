cb47004d5c0e69dc25724817982da4bdb21beb6d
anashishkina <anaschishkina@gmail.com>
2023-06-13 21:40:26 +0200

    Week 20 HW

--- src/main/java/ru/itgirl/libraryproject/model/Book.java
+++ src/main/java/ru/itgirl/libraryproject/model/Book.java
@@ -1,16 +1,14 @@
 package ru.itgirl.libraryproject.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Setter
 @Getter
 @Entity
 public class Book {
